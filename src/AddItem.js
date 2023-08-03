1d130dbfb5450b0381af81bb160af8ba6902b92f
Martin Araka <arakamartin@gmail.com>
2023-08-03 10:18:06 +0300

    apply controlled component input

---
+++ src/AddItem.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react'
+import {FaPlus} from 'react-icons/fa'
+import {useRef} from 'react'
+
+const AddItem = ({newItem,setNewItem, handleSubmit}) => {
+ const inputRef = useRef();
+  return (
+    <form className='addForm' onSubmit = {handleSubmit}>
+        <label htmlFor='addItem'>AddItem</label>
+          <input
+               autoFocus
+               ref={inputRef}
+               id='addItem'
+             
+               type='text'
+               placeholder='Add Item'
+               required
+               value={newItem}
+               onChange={(e) => setNewItem(e.target.value)}
+          />    
+          <button
+               type = 'submit'
+               aria-label = 'Add item'
+               onClick={()=>inputRef.current.focus()}
+             
+          >
+                <FaPlus/>
+          </button>   
+    </form>
+  )
+}
+
+export default AddItem
